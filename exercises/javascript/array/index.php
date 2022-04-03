49fe1f63302f4ae1bd5d9ea8eaa8882b1c6bda8d
Erik <kallgrenerik@gmail.com>
2022-04-03 14:25:27 +0200

    yeah

---
+++ exercises/javascript/array/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JSArrayer</title>
+</head>
+<body>
+	<h1>Uppgifter</h1>
+<a href="array1.html">Uppgift 1</a><br>
+<a href="array2.html">Uppgift 2</a><br>
+<a href="array3.html">Uppgift 3</a><br>
+<a href="array4.html">Uppgift 4</a><br>
+<a href="array5.html">Uppgift 5</a><br>
+<a href="array6.html">Uppgift 6</a><br>
+</body>
+</html>
